4d96884721507a84d99cbb29c99e12d3a3d1262b
motovmp1 <vinicius.mpinho@gmail.com>
2021-01-13 11:00:10 +0000

    Second commit

--- Aula01PullExampleGitHub/Program.cs
+++ Aula01PullExampleGitHub/Program.cs
@@ -9,12 +9,16 @@ namespace Aula01PullExampleGitHub
 {
     class Program
     {
+        /// <summary>
+        /// Comentarios
+        /// </summary>
         static void Main()
         {
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Hello");
-                Thread.Sleep(500);
+                Thread.Sleep(400);
+                Thread.Sleep(100);
                 
             }
             Console.ReadKey();
